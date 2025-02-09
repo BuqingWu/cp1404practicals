d2f5317046fddc1d6aece870320569d11992d164
BuqingWu <buqing.wu@my.jcu.edu.au>
2025-02-09 21:36:21 +0800

    Add prac 2 files

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -12,20 +12,27 @@ def main():
         elif choice == "2":
             print_line_of_asterisks()
         elif choice == "3":
-            print(random.randint)
+            print_random_number()
         else:
             print("Invalid choice")
         print(MENU)
         choice = input("Enter your choice in menu(1/2/3/quit)")
+    print(f"Farewell {name}.")
 
 def get_valid_name():
     """Prompt the user to enter a name and return a valid name after verification."""
     name = input("Enter your name: ")
     if name == " ":
+        return name
+    else:
         name = input("Enter your name: ")
 
 def print_line_of_asterisks():
     """Print a line of asterisks."""
     print("*" * 15)
+
+def print_random_number():
+    """Print a random number(1, 100)"""
+    print("Random number:", random.randint(1,100))
 
 main()
