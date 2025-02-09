31de1af1c1255e84212af6505e0cdab32b4d2c0e
BuqingWu <buqing.wu@my.jcu.edu.au>
2025-02-09 21:42:16 +0800

    Add prac 2 files

---
+++ prac_02/score.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+def main():
+    check_score()
+
+def check_score():
+    """check user's score"""
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
+    else:
+        if score > 90:
+            print("Excellent")
+        elif score < 50:
+            print("Bad")
+        else:
+            print("Passable")
+
+main()
