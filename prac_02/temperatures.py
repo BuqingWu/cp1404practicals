68454cc472d0c039924e9327a9a7a3ced8dcc3b6
BuqingWu <buqing.wu@my.jcu.edu.au>
2025-02-09 22:34:19 +0800

    Add prac 2 files

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -14,22 +14,22 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            convert_Celsius_to_Fahrenheit()
+            convert_celsius_to_fahrenheit()
         elif choice == "F":
-            convert_Fahrenheit_to_Celsius()
+            convert_fahrenheit_to_celsius()
         else:
             print("Invalid option")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
 
-def convert_Celsius_to_Fahrenheit():
+def convert_celsius_to_fahrenheit():
     """Convert Celsius to Fahrenheit"""
     celsius = float(input("Celsius: "))
     fahrenheit = celsius * 9.0 / 5 + 32
     print(f"Result: {fahrenheit:.2f} F")
 
-def convert_Fahrenheit_to_Celsius():
+def convert_fahrenheit_to_celsius():
     """Convert Fahrenheit to Celsius"""
     fahrenheit = float(input("Fahrenheit: "))
     celsius = 5 / 9 * (fahrenheit - 32)
