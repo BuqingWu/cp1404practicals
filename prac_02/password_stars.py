4ab0f59aa98f8df071cb6c9cff0c7e58e6a21719
BuqingWu <buqing.wu@my.jcu.edu.au>
2025-02-16 20:52:30 +0800

    Add prac 2 files

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,21 +1,3 @@
-"""Module docstring"""
-
-# imports
-# CONSTANTS
-
-def main():
-    """Function docstring"""
-    # statements...
-    do_stuff()
-
-
-def do_stuff():
-    """Function docstring"""
-    # statements...
-
-
-main()
-
 PASSWORD_LENGTH = 10
 
 password = input("Enter password: ")
