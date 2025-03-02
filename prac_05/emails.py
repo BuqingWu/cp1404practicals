ec7c0d85c26f5ae4025b35a20313f8a5f4490598
BuqingWu <buqing.wu@my.jcu.edu.au>
2025-03-02 21:35:52 +0800

    Add prac 5 files

---
+++ prac_05/emails.py
@@ -0,0 +1,24 @@
+"""
+Estimate: 30 minutes
+Actual: 88 minutes
+"""
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = extract_name_from_email(email)
+        correct_name = input(f"Is your name {name}? (Y/n): ").strip().lower()
+        if correct_name != "" and correct_name != "y":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+def extract_name_from_email(email):
+    name_part = email.split('@')[0]
+    name = ' '.join(name_part.split('.')).title()
+    return name
+
+main()
+
---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,30 @@
+"""
+Estimate: 30 minutes
+Actual: 65 minutes
+"""
+def main():
+    count_words(text)
+
+def count_words(text):
+    words = text.split()
+    word_counts = {}
+
+# Count the number of occurrences of each word
+    for word in words:
+        word = word.lower()
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+# Sorting a dictionary
+    sorted_word_counts = dict(sorted(word_counts.items()))
+
+# Find the length of the longest word and adjust the output alignment
+    max_word_length = max(len(word) for word in sorted_word_counts)
+    for word, count in sorted_word_counts.items():
+        print(f"{word:{max_word_length}} : {count}")
+
+text = input("Enter a string: ")
+
+main()
